9b8c9b577df886b6e854688f4e10fc898cbf25a8
Campos <100108519+camposeduardo@users.noreply.github.com>
2025-01-27 17:10:40 -0300

    feat: champion entity

---
+++ backend/src/main/java/com/camposeduardo/guess/entity/Champion.java
@@ -0,0 +1,14 @@
+package com.camposeduardo.guess.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Champion {
+
+    private String id;
+    private String name;
+    private String lore;
+
+}
