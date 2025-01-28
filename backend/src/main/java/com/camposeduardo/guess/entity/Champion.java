d3383126a39b2336ece97e3f4aa7fa765fb5dd77
Campos <100108519+camposeduardo@users.noreply.github.com>
2025-01-28 00:10:52 -0300

    refactor: lombok annotation removed, pom.xml

--- backend/src/main/java/com/camposeduardo/guess/entity/Champion.java
+++ backend/src/main/java/com/camposeduardo/guess/entity/Champion.java
@@ -1,14 +1,42 @@
 package com.camposeduardo.guess.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Getter
-@Setter
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Champion {
 
     private String id;
     private String name;
     private String lore;
 
+    public Champion(String id, String name, String lore) {
+        this.id = id;
+        this.name = name;
+        this.lore = lore;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLore() {
+        return lore;
+    }
+
+    public void setLore(String lore) {
+        this.lore = lore;
+    }
 }
