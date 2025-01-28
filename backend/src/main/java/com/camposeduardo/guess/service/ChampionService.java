c26a0a1560274fee94170b01c1934d2fe2a768cf
Campos <100108519+camposeduardo@users.noreply.github.com>
2025-01-28 18:11:37 -0300

    refactor: sending only the lore, variable/function names

--- backend/src/main/java/com/camposeduardo/guess/service/ChampionService.java
+++ backend/src/main/java/com/camposeduardo/guess/service/ChampionService.java
@@ -9,7 +9,6 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 @Service
 public class ChampionService {
@@ -20,11 +19,11 @@ public class ChampionService {
     @Autowired
     private ObjectMapper objectMapper;
 
-
-    public Champion generateRandomChampion() {
+    public String generateRandomChampionLore() {
         List<String> champions = getAllChampionsName();
-        String randomIndex = getRandomChampion(champions);
-        return getChampionInfo(randomIndex);
+        // JarvanIV and Nunu there's a match problem in the response from the API, fix it later
+        String champion = getRandomChampion(champions);
+        return getChampionLore(champion);
     }
 
     public List<String> getAllChampionsName() {
@@ -34,18 +33,28 @@ public class ChampionService {
         return names;
     }
 
-    public Champion getChampionInfo(String championName) {
+    public String getChampionLore(String championName) {
         String url = "https://ddragon.leagueoflegends.com/cdn/15.2.1/data/en_US/champion/%s.json";
         String URI = String.format(url, championName);
 
         Map<String, Object> response = (Map<String, Object>) restTemplate.getForObject(URI, Map.class).get("data");
         Map<String, Object> championDetails = (Map<String, Object>) response.get(championName);
 
-        return objectMapper.convertValue(championDetails, Champion.class);
+        Champion champion = objectMapper.convertValue(championDetails, Champion.class);
+        String championLore = champion.getLore();
+        String toReplace = null;
+
+        if (champion.getName().contains(" ")) {
+            toReplace = champion.getName();
+        } else {
+            toReplace = champion.getId();
+        }
+
+        // removing the champion's name from the lore
+        return championLore.replaceAll(toReplace, "*".repeat(toReplace.length()));
     }
 
     public String getRandomChampion(List<String> champions) {
-        Random generator = new Random();
         int index = (int) (Math.random() * champions.size() + 1);
         return champions.get(index);
     }
