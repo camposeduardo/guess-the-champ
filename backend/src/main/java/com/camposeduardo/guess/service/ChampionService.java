5ef3d496d86f9521b8c9c53f287b28fcb46caaf1
Campos <100108519+camposeduardo@users.noreply.github.com>
2025-01-29 19:56:08 -0300

    refactor: urls in application properties file

--- backend/src/main/java/com/camposeduardo/guess/service/ChampionService.java
+++ backend/src/main/java/com/camposeduardo/guess/service/ChampionService.java
@@ -3,6 +3,7 @@ package com.camposeduardo.guess.service;
 import com.camposeduardo.guess.entity.Champion;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,26 +19,29 @@ public class ChampionService {
 
     @Autowired
     private ObjectMapper objectMapper;
+    @Value("${ddragon.url.allchampions}")
+    private String url_allchampions;
+
+    @Value("${ddragon.url.singlechampion}")
+    private String url_singlechampion;
 
     public String generateRandomChampionLore() {
         List<String> champions = getAllChampionsName();
-        // JarvanIV and Nunu there's a match problem in the response from the API, fix it later
+        // JarvanIV, Nunu, Velkoz there's a match problem in the response from the API, fix it later
         String champion = getRandomChampion(champions);
         return getChampionLore(champion);
     }
 
     public List<String> getAllChampionsName() {
-        String URI = "https://ddragon.leagueoflegends.com/cdn/15.2.1/data/en_US/champion.json";
-        Map<String, Object> response = (Map<String, Object>) restTemplate.getForObject(URI, Map.class).get("data");
+        Map<String, Object> response = (Map<String, Object>) restTemplate.getForObject(url_allchampions, Map.class).get("data");
         List<String> names = new ArrayList<>(response.keySet());
         return names;
     }
 
     public String getChampionLore(String championName) {
-        String url = "https://ddragon.leagueoflegends.com/cdn/15.2.1/data/en_US/champion/%s.json";
-        String URI = String.format(url, championName);
+        String URL = String.format(url_singlechampion, championName);
 
-        Map<String, Object> response = (Map<String, Object>) restTemplate.getForObject(URI, Map.class).get("data");
+        Map<String, Object> response = (Map<String, Object>) restTemplate.getForObject(URL, Map.class).get("data");
         Map<String, Object> championDetails = (Map<String, Object>) response.get(championName);
 
         Champion champion = objectMapper.convertValue(championDetails, Champion.class);
